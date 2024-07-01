d911a03d5ed069d094a89726af1314729e6139b1
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2024-07-01 13:39:09 +0530

    Create IterativeInorder_trees.java

---
+++ IterativeInorder_trees.java
@@ -0,0 +1,61 @@
+import java.util.*;
+        
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int key){
+        data=key;
+        left=null;
+        right=null;
+    }
+}
+
+class BinaryTree{
+    Node root;
+
+    BinaryTree(){
+        root=null;
+    }
+
+    void printInOrder(Node node){
+        if (node==null) System.out.println("Tree root is null");
+        else{
+            Stack<Node> stack = new Stack<>();
+
+            while(true){
+                if (node != null){
+                    stack.push(node);
+                    node=node.left;
+                }
+                else{
+                    if(stack.isEmpty()) break;
+                    node=stack.pop();
+                    System.out.print(node.data+" "); //basically it is the root when left node was null and also one of the left node present in the left subtree.
+                    node=node.right; //and now going to right node
+                }
+            }
+        }
+    }
+}
+
+public class IterativeInorder_trees {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(50);
+        tree.root.left = new Node(25);
+        tree.root.right = new Node(75);
+        tree.root.left.left = new Node (12);
+        tree.root.left.right = new Node(37);
+        tree.root.right.left = new Node(62);
+        tree.root.right.right = new Node(57);
+        tree.root.left.right.left = new Node(30);
+        tree.root.left.right.right = new Node(40);
+        tree.root.right.left.left = new Node(60);
+        tree.root.right.left.right = new Node(70);
+
+        tree.printInOrder(tree.root);
+    }
+}
